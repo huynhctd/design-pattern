02dee4e819e59e22800e1e06957209c204de4125
huynhctd <huynhctd@smartosc.com>
2023-02-25 22:29:39 +0700

    commit gitignore

--- src/decorator_pattern/beverage/Espresso.java
+++ src/decorator_pattern/beverage/Espresso.java
@@ -4,11 +4,9 @@ import decorator_pattern.Beverage;
 
 public class Espresso extends Beverage {
     public Espresso(Beverage beverage) {
-        super(beverage);
     }
 
     public Espresso(){
-        super();
         description = "Espresso";
     }
 
--- src/decorator_pattern/beverage/HouseBlend.java
+++ src/decorator_pattern/beverage/HouseBlend.java
@@ -4,12 +4,7 @@ import decorator_pattern.Beverage;
 
 public class HouseBlend extends Beverage {
     public HouseBlend() {
-        super();
         description = "HouseBlend";
-    }
-
-    public HouseBlend(Beverage beverage) {
-        super(beverage);
     }
     @Override
     public double cost() {
